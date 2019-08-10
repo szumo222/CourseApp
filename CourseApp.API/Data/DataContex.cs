3706de6fdc256a7f597d9bfe37d060386117fd96
szumo222 <szumo22@gmail.com>
2019-08-11 00:55:44 +0200

    Initial Commit

---
+++ CourseApp.API/Data/DataContex.cs
@@ -0,0 +1,12 @@
+using CourseApp.API.Modules;
+using Microsoft.EntityFrameworkCore;
+
+namespace CourseApp.API.Data
+{
+    public class DataContex : DbContext
+    {
+        public DataContex(DbContextOptions<DataContex> options) : base (options) {}
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
---
+++ CourseApp.API/Modules/Value.cs
@@ -0,0 +1,8 @@
+namespace CourseApp.API.Modules
+{
+    public class Value
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+}
