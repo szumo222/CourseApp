f70850425ce5618f89c707fffbaab38e36637caa
szumo222 <szumo22@gmail.com>
2019-08-19 19:18:40 +0200

    End of section 4 - client side of user login/register

--- CourseApp.API/Controllers/ValuesController.cs
+++ CourseApp.API/Controllers/ValuesController.cs
@@ -22,6 +22,7 @@ namespace CourseApp.API.Controllers
 
         }
         // GET api/values
+        [AllowAnonymous]
         [HttpGet]
         public async Task<IActionResult> GetValues()
         {
